19232e385d767f0bcd14c3c9252469b327224722
Moien Makkiyan <moien.makkiyan@gmail.com>
2022-01-25 14:11:07 +0330

    1/25/2022 9:42:03

--- MoienMakkiyan_99102264_final.v
+++ MoienMakkiyan_99102264_final.v
@@ -14,5 +14,24 @@ module mymodule(in1,in2,Clock,reset,out);
         stack[2]=1;
         stack[3]=1;
     end
-    
+    always @(posedge Clock or reset) begin
+        if (reset == 0) begin
+            out = 3'b000;
+        end
+        else begin
+            if (in1 == in2 && isInitialized ==0) begin
+                counter = counter + 4'b0001;
+                stack[counter] = 1;
+            end
+            else begin
+                counter = 4'b0000;
+            end
+            if (counter == 4'b0100) begin
+                isInitialized = 1;
+            end
+            if (isInitialized == 1) begin
+                
+            end
+        end
+    end
 endmodule
